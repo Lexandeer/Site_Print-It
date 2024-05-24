f5040be21d694e0056cfc0ae69aa835e0872de2e
Alexandre <alexmiquel888@gmail.com>
2024-05-24 11:06:26 +0200

    Ajustement en vue de la soutenance

--- assets/script.js
+++ assets/script.js
@@ -21,52 +21,54 @@ let dots = document.querySelector("#banner .dots");
 let p = document.querySelector("#banner p");
 let img = document.querySelector("#banner img.banner-img");
 
-function addDot() {										//Fonction pour ajouter mes dots.
+
+let currentIndex = 0;										//Compteur
+
+
+function addDot() {											//Fonction pour ajouter mes dots.
 	for(let i = 0; i < slides.length; i++) {
 		let dot = document.createElement('div');
 		dot.className = 'dot';
 		dots.appendChild(dot);
 	}
 }
-document.addEventListener('DOMContentLoaded', function() {  //J'appelle ma fonction pour ajouter les dots, au chargement de la page.
-	addDot();
-	slideUpDate(currentIndex);
-})
 
 
-
-
-let currentIndex = 0;
-
-function slideUpDate(currentIndex){
+function slideUpDate(currentIndex){							//Fonction Pour changer de slide et changer la "dot_selected"
 	img.setAttribute("src", slides[currentIndex].image);
 	p.innerHTML = slides[currentIndex].tagLine;
 
 	let dot = document.querySelectorAll("#banner .dot");
+
 	for(let i = 0; i < slides.length; i++) {
 		if(i === currentIndex){
 			dot[i].classList.add('dot_selected');
 		} else {
 			dot[i].classList.remove('dot_selected');
 		}
-
 	}
 }
 
-let arrowRight = document.querySelector("#banner button.arrow_right");		
-	arrowRight.addEventListener("click",() => {									//EventListener pour la flèche de droite.
+document.addEventListener('DOMContentLoaded', function() {  //J'appelle mes fonction au chargement de la page.
+	addDot();
+	slideUpDate(currentIndex);
+})
+
+
+let arrowRight = document.querySelector("#banner button.arrow_right");		//EventListener pour la flèche de droite.	
+	arrowRight.addEventListener("click",() => {									
 		currentIndex += 1;
-			if (currentIndex === 4) {
+			if (currentIndex === 4) {										//Defilement infini
 				currentIndex = 0;
 			}
 		slideUpDate(currentIndex);
 	})
 
 
-let arrowLeft = document.querySelector("#banner button.arrow_left");
-	arrowLeft.addEventListener("click",() => {									//EventListener pour la flèche de gauche.
+let arrowLeft = document.querySelector("#banner button.arrow_left");		//EventListener pour la flèche de gauche.
+	arrowLeft.addEventListener("click",() => {									
 		currentIndex -= 1;
-		if (currentIndex === -1) {
+		if (currentIndex === -1) {											//Defilement infini
 			currentIndex = 3;
 		}
 		slideUpDate(currentIndex);
